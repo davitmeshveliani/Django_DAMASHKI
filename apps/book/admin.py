cd0b41434bbebeea02083acc1071df576b5fb559
davitmeshveliani <davitmeshveliani@gmail.com>
2026-07-26 09:33:08 +0200

    add DAMASHK_11

--- apps/book/admin.py
+++ apps/book/admin.py
@@ -1,19 +1,41 @@
 from django.contrib import admin
 from .models import Task, SubTask, Category
 
+
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
 
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'created_at')
+    list_display = ('short_title', 'category', 'created_at')
     list_filter = ('category', 'created_at')
     search_fields = ('title',)
+    inlines = (SubTaskInline,)
+
+    def short_title(self, obj):
+        if len(obj.title) > 10:
+            return obj.title[:10] + '***'
+        return obj.title
+
+    short_title.short_description = 'Title'
+
 
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'task', 'created_at')
     list_filter = ('task',)
     search_fields = ('title',)
+    actions = ['make_done']
+
+    @admin.action(description='Mark selected subtasks as Done')
+    def make_done(self, request, queryset):
+        updated_count = queryset.update(status='Done')
+        self.message_user(request, f"{updated_count} subtasks were successfully marked as Done.")
--- apps/myapp/admin.py
+++ apps/myapp/admin.py
@@ -2,16 +2,41 @@ from django.contrib import admin
 from .models import Task, SubTask, Category
 
 
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'deadline', 'created_at')
-    filter_horizontal = ('categories',)
-    list_filter = ('status', 'deadline')
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1
 
-@admin.register(SubTask)
-class SubTaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'task', 'status', 'deadline')
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('short_title', 'status', 'deadline', 'created_at')
+    filter_horizontal = ('categories',)
+    list_filter = ('status', 'deadline')
+    search_fields = ('title',)
+    inlines = (SubTaskInline,)
+
+    def short_title(self, obj):
+        if len(obj.title) > 10:
+            return obj.title[:10] + '...'
+        return obj.title
+
+    short_title.short_description = 'Title'
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'status', 'deadline')
+    list_filter = ('status', 'deadline')
+    search_fields = ('title',)
+    actions = ['make_done']
+
+    @admin.action(description='Mark selected subtasks as Done')
+    def make_done(self, request, queryset):
+        updated_count = queryset.update(status='Done')
+        self.message_user(request, f"{updated_count} subtasks were successfully marked as Done.")
