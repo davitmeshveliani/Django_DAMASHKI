b82eb4e8eeb3eed6816ae3324d25098618b3c675
davitmeshveliani <davitmeshveliani@gmail.com>
2026-08-08 13:06:16 +0200

    add DAMASHK_12

--- apps/myapp/urls.py
+++ apps/myapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from apps.myapp.views.task_views import TaskListCreateView, TaskDetailView
 #from apps.myapp.views.stats_views import TaskStatsView
+from apps.myapp.views import SubTaskListCreateView, SubTaskDetailUpdateDeleteView
 
 urlpatterns = [
     path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
 #    path('tasks/stats/', TaskStatsView.as_view(), name='task-stats'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+
+    path('subtasks/', SubTaskListCreateView.as_view(), name='subtask-list-create'),
+    path('subtasks/<int:pk>/', SubTaskDetailUpdateDeleteView.as_view(), name='subtask-detail')
 ]
