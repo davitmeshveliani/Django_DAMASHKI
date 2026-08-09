84866b38aef540971d6c18d2add26223851e7cce
davitmeshveliani <davitmeshveliani@gmail.com>
2026-08-09 12:34:03 +0200

    add DAMASHK_13

--- apps/myapp/views/task_views.py
+++ apps/myapp/views/task_views.py
@@ -9,8 +9,11 @@ from apps.serializers.my_app_model_serializers import (
     TaskDetailSerializer,
 )
 
-class TaskListCreateView(APIView):
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 1. აქტიური კოდი: APIView (მანუალური მიდგომა)
+# /~~~~~~~~~~~~~~~~~~~~~~~~~
 
+class TaskListCreateView(APIView):
     def get(self, request):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
@@ -23,9 +26,7 @@ class TaskListCreateView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 class TaskDetailView(APIView):
-
     def get(self, request, pk):
         task = get_object_or_404(Task, pk=pk)
         serializer = TaskDetailSerializer(task)
@@ -52,22 +53,10 @@ class TaskDetailView(APIView):
         task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-
-
-
-
-#       generics  avtomatisirt
-
-
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 2. დაკომენტარებული ალტერნატივა: GENERICS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # from rest_framework import generics
-# from apps.myapp.models import Task, SubTask
-# from apps.serializers.my_app_model_serializers import (
-#     TaskSerializer,
-#     TaskCreateSerializer,
-#     TaskDetailSerializer,
-# )
 #
 # class TaskListCreateView(generics.ListCreateAPIView):
 #     queryset = Task.objects.all()
@@ -77,11 +66,25 @@ class TaskDetailView(APIView):
 #             return TaskCreateSerializer
 #         return TaskSerializer
 #
-#
 # class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Task.objects.all()
 #
 #     def get_serializer_class(self):
 #         if self.request.method in ['PUT', 'PATCH']:
+#             return TaskCreateSerializer
+#         return TaskDetailSerializer
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3. დაკომენტარებული ალტერნატივა: VIEWSET
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# from rest_framework import viewsets
+#
+# class TaskViewSet(viewsets.ModelViewSet):
+#     queryset = Task.objects.all()
+#
+#     def get_serializer_class(self):
+#         if self.request.method == 'POST':
+#             return TaskCreateSerializer
+#         elif self.request.method in ['PUT', 'PATCH']:
 #             return TaskCreateSerializer
 #         return TaskDetailSerializer
--- apps/serializers/my_app_model_serializers.py
+++ apps/serializers/my_app_model_serializers.py
@@ -1,32 +1,29 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
-from django.utils import timezone
 from apps.myapp.models import Category, Task, SubTask
-
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name']
 
+class TaskSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True, read_only=True)
 
-# Задание 2: CategoryCreateSerializer с проверкой уникальности названия
-class CategoryCreateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Category
-        fields = ['id', 'name']
+        model = Task
+        fields = ['id', 'title', 'description', 'categories', 'status', 'deadline', 'created_at']
 
-    def create(self, validated_data):
-        name = validated_data.get('name')
-        if Category.objects.filter(name=name).exists():
-            raise ValidationError({'name': 'Категория с таким названием уже существует.'})
-        return super().create(validated_data)
+class TaskDetailSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True, read_only=True)
 
-    def update(self, instance, validated_data):
-        name = validated_data.get('name')
-        if name and name != instance.name and Category.objects.filter(name=name).exists():
-            raise ValidationError({'name': 'Категория с таким названием уже существует.'})
-        return super().update(instance, validated_data)
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'categories', 'status', 'deadline', 'created_at']
+
+class TaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'categories', 'status', 'deadline']
 
 
 class SubTaskSerializer(serializers.ModelSerializer):
@@ -34,41 +31,7 @@ class SubTaskSerializer(serializers.ModelSerializer):
         model = SubTask
         fields = ['id', 'title', 'description', 'task', 'status', 'deadline', 'created_at']
 
-
-# Задание 1: SubTaskCreateSerializer (created_at только для чтения)
 class SubTaskCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubTask
-        fields = ['id', 'title', 'description', 'task', 'status', 'deadline', 'created_at']
-        read_only_fields = ['created_at']
-
-
-# Задание 3: TaskDetailSerializer с вложенными подзадачами
-class TaskDetailSerializer(serializers.ModelSerializer):
-    categories = CategorySerializer(many=True, read_only=True)
-    subtasks = SubTaskSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Task
-        fields = ['id', 'title', 'description', 'categories', 'subtasks', 'status', 'deadline', 'created_at']
-
-
-# Задание 4: TaskCreateSerializer с валидацией дедлайна
-class TaskCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = ['id', 'title', 'description', 'status', 'deadline']
-
-    def validate_deadline(self, value):
-        if value < timezone.now():
-            raise ValidationError('Дата дедлайна не может быть в прошлом.')
-        return value
-
-
-class TaskSerializer(serializers.ModelSerializer):
-    categories = CategorySerializer(many=True, read_only=True)
-    subtasks = SubTaskSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Task
-        fields = ['id', 'title', 'description', 'categories', 'subtasks', 'status', 'deadline', 'created_at']
+        fields = ['id', 'title', 'description', 'task', 'status', 'deadline']
